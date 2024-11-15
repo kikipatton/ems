e2f2e1a798013df4485bd262963e5f05354e72ed
kikipatton <kiki@neesites.co.ke>
2024-11-15 19:10:58 +0300

    bug fix

--- ems/urls.py
+++ ems/urls.py
@@ -23,5 +23,26 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
+        path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='auth/password_reset.html',
+        html_email_template_name='auth/password_reset_email.html',
+        subject_template_name='auth/password_reset_subject.txt'
+    ), name='password_reset'),
+    
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='auth/password_reset_done.html'
+    ), name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='auth/password_reset_confirm.html',
+        success_url=reverse_lazy('login'),
+        post_reset_login=True,
+    ), name='password_reset_confirm'),
+    
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='auth/password_reset_complete.html'
+    ), name='password_reset_complete'),
     path('', include('house.urls')),
 ]
+
+
