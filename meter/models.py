1c4ae8090995cd366dbea9c419d1f72918c7fe20
kikipatton <kiki@neesites.co.ke>
2024-11-27 00:05:22 +0300

    up

--- meter/models.py
+++ meter/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from house.models import House  # Assuming House model is in estates app
+from house.models import House 
 
 class Meter(models.Model):
     STATUS_CHOICES = [
